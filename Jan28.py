bc632388bc056a8493935690a0cc8aed5e58a268
Pa <py20@duke.edu>
2015-01-28 16:17:51 -0500

    update file

--- Jan28.py
+++ Jan28.py
@@ -1,6 +1,82 @@
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
- 
+import os
+
 main_dir = "/Users/Pa/BigData/Data"
 git_dir = "/Users/Pa/Desktop/2015Spring/PUBPOL590/Pa_Ye"
+csv_file_good = "sample_data_clean.csv"
+csv_file_bad = "/sample_data_clean.csv"
+
+# OS MODULE----------------------
+
+df = pd.read_csv(os.path.join(main_dir,csv_file_good))
+df = pd.read_csv(os.path.join(main_dir,csv_file_bad))
+
+# PYTHON DATA TYPES
+
+## strings
+str1 = "hello, computer"
+str2 = 'hello, human'
+str3 = u'eep
+
+type(str1) #type str
+type(str2)
+type(str3) #type unicode
+
+## numeric
+int1 = 10
+float1 = 20.56
+long1 = 289982904790009999999999999999999999999999
+
+## logical
+bool1 = True
+notbool1 = 0
+bool2 = bool(notbool1)
+
+# CREATING LISTS AND TYPLES----------------------------
+
+## in brief, list can be changed, tuples cannot
+## we almost exclusively use lists
+
+list1 = []
+list1
+list2 = [7,8,"a"]
+list2[2] # output "a"
+list2[2] = 5
+
+## tuples, cannot change
+tup1 = (8, 3, 19)
+tup1[2] # output is 19
+tup1[2] = 5
+
+## convert
+list2 = list(tup1)
+tup2 = tuple(list1)
+
+## list can be append and extended
+list2.append([3,90]) # out: [8,3,19,[3,90]]
+len(list2)
+list3 = [8,3,19]
+list3.extend([6,88]) # out: [8,3,19,6,88]
+len(list3)
+
+# CONVERTYING LISTS TO SERIES AND DATAFRAME
+list4 = range(5,10) # range(n,m)--gives a list from
+                    # n to m-1
+list4 # out [5, 6, 7, 8, 9]
+list5 = range(5) # range(m)--gives list from 0 to m-1
+list5 # out [0, 1, 2, 3, 4]
+list6 = ['q','r','s','t','u']
+
+## list to series
+s1 = Series(list4)
+s2 = Series(list5)
+s3 = Series(list6)
+
+## lists to DataFrame
+
+dataframe1=DataFrame([s1])
+
+
+
